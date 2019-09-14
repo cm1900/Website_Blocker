6fb5ddccc944611e36bfcc27dff9d35eb54ab648
cm1900 <cmalebe139@gmail.com>
2019-09-14 07:22:08 +0200

    Compare current time to working hours

--- main.py
+++ main.py
@@ -1,9 +1,14 @@
 import time
+from datetime import datetime as dt
+
 hosts_path = r'C:\Windows\System32\drivers\etc\hosts'
 redirect = '127.0.0.1'
 blocked_sites = ['www.facebook.com', 'facebook.com', 'www.twitter.com',
 'twitter.com']
 
 while True:
-
+    if dt(dt.now().year, dt.now().month, dt.now().day, 8) < dt.now() < dt(dt.now().year, dt.now().month, dt.now().day, 17):
+        print("Working hours...")
+    else:
+        print("Offline...")
     time.sleep(10)
