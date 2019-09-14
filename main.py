2bc8d049faa0d56be15da65ca9792be07a0c46e7
cm1900 <cmalebe139@gmail.com>
2019-09-14 07:05:58 +0200

    Setting up infinite loop

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
+import time
 hosts_path = r'C:\Windows\System32\drivers\etc\hosts'
 redirect = '127.0.0.1'
 blocked_sites = ['www.facebook.com', 'facebook.com', 'www.twitter.com',
 'twitter.com']
+
+while True:
+
+    time.sleep(10)
